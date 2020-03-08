be94155e8f639596b6d7f7524f4800cf1c956994
Abh15h3k <abhishekbanerji1999@gmail.com>
2020-03-08 11:33:59 +0530

    proper symbol table

---
+++ symbol_table/test1.c
@@ -0,0 +1,24 @@
+//without error - nested if-else
+#include<stdio.h>
+#define x 3
+int main()
+{
+	int a=4;
+	int i;
+	if(a<10)
+	{
+		a = a + 1;
+	}
+	else
+	{
+		a = a + 2;
+	}
+	printf("%d", b)
+	while(a<10)
+	{
+		a++;
+	}
+	for(i=1;i<5;i++)
+		a--;
+	return;
+}
