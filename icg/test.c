88b99d69bed6c0edccc1f7b1c47cb1828a10e316
Abh15h3k <abhishekbanerji1999@gmail.com>
2020-04-10 22:53:46 +0530

    almost working

---
+++ icg/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    a = a + 5
+
+    while( a != 0 )
+        a = a - 2;
+
+    return 0;
+}
+
+
---
+++ icg/test1.c
@@ -0,0 +1,19 @@
+//no error
+#include <stdio.h>
+
+int foo(int a)
+{
+	a=a+1;
+	return a;
+}
+
+void main()
+{
+        int a;
+	int b;
+	int c;
+	b=5;
+	int c;
+	c=foo(b);
+	return;
+}
