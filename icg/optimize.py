c81ea6693537378b43839ecf0ee1703b21a3249f
Abh15h3k <abhishekbanerji1999@gmail.com>
2020-05-14 10:37:42 +0530

    small oopsie

--- icg/optimize.py
+++ icg/optimize.py
@@ -1,188 +1,222 @@
-import re
 import sys
+import operator
+import re
 
-if len(sys.argv) != 2: 
-    print("Correct usage: Enter filename\n")
-    exit()
+ops = { "+": operator.add,
+        "-": operator.sub,
+        "*": operator.mul,
+        "/": operator.floordiv}
 
-icg_file = sys.argv[1]
+def printLines(arr):
+    for line in arr:
+        print(line)
+    print("\n")
 
-# icg_file = "Output/icg.txt"
+num="(\d+(?:\.\d+)?)"
+loop=0
+loop_end="unset"
+def match(line):
 
-istemp = lambda s : bool(re.match(r"^t[0-9]*$", s)) 		#temporary variable
-isid = lambda s : bool(re.match(r"^[A-Za-z][A-Za-z0-9_]*$", s)) #id + temporary variable
-
-binary_operators = {"+", "-", "*", "/", "*", "&", "|", "^", "==", ">=", "<=", "!=", ">", "<"}
-
-def printicg(list_of_lines, message = "") :
-	print(message.upper())
-	for line in list_of_lines :
-		print(line.strip())
-
-
-def eval_wrap(line) :
-	tokens = line.split()
-	if len(tokens) != 5 :
-		return line
-	if tokens[1] != "=" or tokens[3] not in binary_operators:
-		return line
-	#tokens = tokens[2:]
-	if tokens[2].isdigit() and tokens[4].isdigit() :
-		result = eval(str(tokens[2] + tokens[3] + tokens[4]))
-		return " ".join([tokens[0], tokens[1], str(result)])
-	if tokens[2].isdigit() or tokens[4].isdigit() : #Replace the identifier with a number and evaluate
-		op1 = "5" if isid(tokens[2]) else tokens[2]
-		op2 = "5" if isid(tokens[4]) else tokens[4]
-		op = tokens[3]
-		try : 
-			result = int(eval(op1+op+op2))
-			if result == 0 : #multiplication with 0
-				return " ".join([tokens[0],tokens[1], "0"])
-			elif result == 5 : #add zero, subtract 0, multiply 1, divide 1
-				if isid(tokens[2]) and tokens[4].isdigit() :
-					return " ".join([tokens[0], tokens[1], tokens[2]])
-				elif isid(tokens[4]) and tokens[2].isdigit():
-					return " ".join([tokens[0], tokens[1], tokens[4]])
-			elif result == -5 and tokens[2] == "0" : # 0 - id
-				return " ".join([tokens[0], tokens[1], "-"+tokens[4]])
-			return " ".join(tokens)
-
-		except NameError :
-			return line
-		except ZeroDivisionError :
-			print("Division By Zero is undefined")
-			quit()
-	return line
-
-
-def fold_constants(list_of_lines) :
-	"""
-	Some expressions that can have a definite answer need not be waste run time resources :
-	e.g.
-	1. number + number, number - number etc.
-	2. identifier + 0, identfier / 0, identifer - 0, identifier*0 and their commutatives
-	3. identifier * 1, identifier / 1
-	"""
-
-	new_list_of_lines = []
-	for line in list_of_lines :
-		new_list_of_lines.append(eval_wrap(line))
-	return new_list_of_lines
-
-def remove_dead_code(list_of_lines) :
-	"""
-Temporaries that are never assigned to any variable nor used in any expression are deleted. Done recursively.
-	"""
-	num_lines = len(list_of_lines)
-	temps_on_lhs = set()
-	for line in list_of_lines :
-		tokens = line.split()
-		if istemp(tokens[0]) :
-			temps_on_lhs.add(tokens[0])
-
-	useful_temps = set()
-	for line in list_of_lines :
-		tokens = line.split()
-		if len(tokens) >= 2 :
-			if istemp(tokens[1]) :
-				useful_temps.add(tokens[1])
-		if len(tokens) >= 3 :
-			if istemp(tokens[2]) :	
-				useful_temps.add(tokens[2])
-
-	temps_to_remove = temps_on_lhs - useful_temps
-	new_list_of_lines = []
-	for line in list_of_lines :
-		tokens = line.split()
-		if tokens[0] not in temps_to_remove :
-			new_list_of_lines.append(line)
-	if num_lines == len(new_list_of_lines) :
-		return new_list_of_lines
-	return remove_dead_code(new_list_of_lines)
-
-"""
-
-# { rhs : lhs }
-a = 3 + 1
-b = 3 + 1
-
-{}
-
-def wrap_temps(list_of_lines, unique_temps = 100) :
-	temps_for_reuse = set()
-	number_of_lines = len(list_of_lines)
-	for i in range(number_of_lines) :
-		tokens = list_of_lines[i].split()
-		if len(tokens) == 5 and istemp(tokens[4]) : #a temp has been assigned to something else
-"""
-def make_subexpression_dict(list_of_lines) :
-	expressions = {}
-	variables = {}
-	for line in list_of_lines :
-		tokens = line.split()
-		if len(tokens) == 5 :
-			# print("variables",variables)
-			if tokens[0] in variables and variables[tokens[0]] in expressions :
-				print("here")
-				print(tokens[0], variables[tokens[0]], expressions[variables[tokens[0]]])
-				del expressions[variables[tokens[0]]]
-			rhs = tokens[2] + " " + tokens[3] + " " + tokens[4]
-			if rhs not in expressions :
-				expressions[rhs] = tokens[0]
-				if isid(tokens[2]) :
-					variables[tokens[2]] = rhs
-				if isid(tokens[4]) :
-					variables[tokens[4]] = rhs
-	return expressions
-
-def eliminate_common_subexpressions(list_of_lines) :
-	expressions = make_subexpression_dict(list_of_lines)
-	print(expressions)
-	lines = len(list_of_lines)
-	new_list_of_lines = list_of_lines[:]
-	for i in range(lines) :
-		tokens = list_of_lines[i].split()
-		if len(tokens) == 5 :
-			rhs = tokens[2] + " " + tokens[3] + " " + tokens[4]
-			if rhs in expressions and expressions[rhs] != tokens[0]:
-				new_list_of_lines[i] = tokens[0] + " " + tokens[1] + " " + expressions[rhs]
-	return new_list_of_lines
-				
-
-if __name__ == "__main__" :
-
-	if len(sys.argv) == 2 :
-		icg_file = str(sys.argv[1])
-	
-	list_of_lines = []
-	f = open(icg_file, "r")
-	for line in f :
-		list_of_lines.append(line)
-	f.close()
-
-	printicg(list_of_lines, "ICG")
-	print("\n")
-
-
-	eliminated_common_subexpressions = eliminate_common_subexpressions(list_of_lines)
-	print("\n")
-	printicg(eliminated_common_subexpressions, "Optimized ICG after eliminating common subexpressions")
-	print("\n")
-
-	folded_constants = fold_constants(eliminated_common_subexpressions)
-	printicg(folded_constants, "Optimized ICG after constant folding")
-	print("\n")
-
-	#testing 
-	without_deadcode = remove_dead_code(folded_constants)
-	printicg(without_deadcode, "Optimized ICG after removing dead code")
-
-	print("\n")
-	print("Eliminated", len(list_of_lines)-len(without_deadcode), "lines of code")
-	print("\n")
-	
-
-#adding dead code in the end
+    global loop
+    global loop_end
+    if not loop:
+        if re.match("t\d+ = "+num+" [\+\-\/\*] "+num+"", line) :
+            return 1
+        elif re.match("t\d+ = [a-z]+ [\+\-\/\*] "+num+"", line) :
+            return 5
+        elif re.match("t\d+ = "+num+" [\+\-\/\*] [a-z]+", line) :
+            return 6
+        elif re.match("t\d* = "+num+"", line) :
+            return 2
+        elif re.match("[a-z]+ = "+num+"", line) :
+            return 3
+        elif re.match("[a-z]+ = t\d*", line) :
+            return 4
+        elif re.match("t\d+ = [a-z]+ [\+\-\/\*] [a-z]+", line) :
+            return 7
+        elif re.match("[a-z]+ = [a-z]+", line):
+            return 4
+        elif re.match("if .* goto .*", line) :
+            loop_end = line.split()[-1]
+            # print("ending:",loop_end);
+            loop = 1
+    elif re.match("[a-z]+.* =",line):
+        # print("in loop:", line)
+        var = line.split()[0]
+        table[var] = "na";
+    elif re.match(loop_end, line):
+        loop = 0
+        loop_end="unset"
 
 
 
+    return 0
+
+def getVal(token):
+    try:
+        val = int(token)
+    except ValueError:
+        val = float(token)
+
+    return val
+
+
+def func1(line):
+    tokens = line.split()
+    val = ops[tokens[3]](getVal(tokens[2]),getVal(tokens[4]))
+    new_line = tokens[0] + " = " + str(val)
+    return new_line
+
+def func2(line):
+    tokens = line.split()
+    table[tokens[0]] = getVal(tokens[2])
+    return ""
+
+
+def func3(line):
+    tokens = line.split()
+    table[tokens[0]] = getVal(tokens[2])
+    return line
+
+def func4(line):
+    new_line = ""
+    # print(line)
+    tokens = line.split()
+    if tokens[2] in table.keys() and table[tokens[2]] != "na":
+        new_line = tokens[0] + " = " + str(table[tokens[2]])
+    if not new_line:
+        return line
+    return new_line
+
+def func5(line):
+    tokens = line.split()
+    if tokens[2] in table.keys() and table[tokens[2]] != "na":
+        new_line = tokens[0] + " = " + str(table[tokens[2]]) + " " + tokens[3] + " " + tokens[4]
+    else:
+        return line
+    return new_line
+
+def func6(line):
+    tokens = line.split()
+    if tokens[4] in table.keys() and table[tokens[4]] != "na":
+        new_line = tokens[0] + " = " + tokens[2] + " " + tokens[3] + " " + str(table[tokens[4]])
+    else:
+        return line
+    # print("??",new_line)
+    return new_line
+
+
+def func7(line):
+    tokens = line.split()
+    new_line = tokens[0] + " = "
+    if tokens[2] in table.keys():
+        new_line += str(table[tokens[2]]) + " + "
+    else:
+        new_line += tokens[2] + " + "
+
+    if tokens[4] in table.keys():
+        new_line += str(table[tokens[4]])
+    else:
+        new_line += tokens[4]
+
+    # print("#7:", new_line)
+    return new_line
+
+
+table = dict()
+
+def main():
+    if len(sys.argv) == 2:
+        icg = str(sys.argv[1])
+
+    lines = []
+    f = open(icg, 'r')
+    for line in f:
+        if line != '\n':
+            lines.append(line.split('\n')[0])
+    f.close()
+
+    print("Input ICG")
+    printLines(lines)
+
+    opt1 = []
+
+    for line in lines:
+        f = match(line)
+        # print(line, ":", f)
+        # print("loop:", loop)
+        while f:
+            if f == 1:
+                nline = func1(line)
+            elif f == 2:
+                nline = func2(line)
+            elif f == 3:
+                nline = func3(line)
+                break
+            elif f == 4:
+                nline = func4(line)
+            elif f == 5:
+                nline = func5(line)
+            elif f == 6:
+                nline = func6(line)
+            elif f == 7:
+                nline = func7(line)
+            else:
+                break
+            if nline == line:
+                break
+            else:
+                line = nline
+            f = match(line)
+            # print(line, ":", f)
+            # print("loop:", loop)
+        if line:
+            opt1.append(line)
+
+    # print(table)
+
+    table2 = dict()
+    temp = []
+    # printLines(opt1)
+
+    for line in opt1:
+        if re.match("[a-z]+ = [a-z]+$",line):
+            tokens = line.split()
+            table2[tokens[0]] = tokens[2]
+        else:
+            temp.append(line)
+    # print(temp)
+
+    opt2 = []
+    for line in opt1:
+        # print("hi:",line)
+        new_line = ""
+        tokens = line.split()
+        i = 2
+        if len(tokens) > 2:
+            new_line = tokens[0] + " " + tokens[1] + " "
+        elif len(tokens) == 2:
+            new_line = tokens[0] + " " + tokens[1] + " "
+        else:
+            new_line = tokens[0] + " "
+        while i < len(tokens):
+            if tokens[i] in table2.keys():
+                new_line += table2[tokens[i]]
+            else:
+                new_line += tokens[i]
+            new_line += " "
+            i += 1
+        # print("before bye:",new_line)
+        new_line = new_line[:-1]
+        # print("bye:",new_line)
+        opt2.append(new_line)
+
+    print("After Optimizing")
+    printLines(opt2)
+
+if __name__ == "__main__":
+    try:
+        main()
+        # print(table)
+    except KeyboardInterrupt:
+        print(table)
